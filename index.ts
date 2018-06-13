cc30977bd0ff410e2623847e3a16a1ed58df1619
László Kovács <laszlo@heven.com>
2018-06-13 13:45:46 +0200

    config to reproduce bug

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
 import { ImportTestInterFace } from './typeDef';
+import moment from 'moment';
+import minimist from 'minimist';
 
 const a: ImportTestInterFace = {test: false};
+const b: moment.BaseMomentLanguage;
+const c: minimist.Opts;
